2302200f74295521662f5d35c5f347cef56f1269
rahafalbakkar <rahaf.a7med1991@gmail.com>
2021-08-14 14:20:27 +0300

    add

--- js /loss-weight.js
+++ js /loss-weight.js
@@ -1,45 +1,48 @@
 'use strict';
 // localStorage.clear();
 
-let arrayImg =['loss3.jpg',
+let imgArray =['loss3.jpg',
 'loss2.jpg',' loss4.jpg',
 'loss6.jpg',' loss5.jpg',
 'loss8.jpg',' loss7.jpg',
 'loss9.jpg','index.jpg'];
 
  
-let sectionOfImg = document.getElementsByClassName('sectionOfImg');
-let rightImg = document.getElementsById('rightSide');
-let leftImg = document.getElementsById('leftSide');
+let all=0;
+let numberOfRound= 8;
+let counter =0;
+
+let sectionOfImg = document.getElementByID('sectionOfImg');
+let leftSide = document.getElementByID('leftSide');
+// let leftImg = document.getElementByClassName('');
 
 function show(imgName, imgSrc) {
     this.imgName = imgName;
-    this.imgSrc = imgSrc;
+    this.image = imgSrc;
     this.shown=0;
         show.all.push(this);
 }
-let all=0;
 show.all = [];
-for (let i=0; i<arrayImg.length;i++){
-    new show(arrayImg[i].split('.')[0],arrayImg[i]);   
+
+for (let i=0; i<imgArray.length;i++){
+    new show(imgArray[i].split('.')[0],imgArray[i]);   
 }
+
 console.log(show.all);
 
-
-function random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
 let firstRandom;
 let sndRandom;
 
 function render(){
-    firstRandom=random(0,arrayImg.length-1);
-    sndRandom=random(0,arrayImg.length-1);
+    firstRandom=random(0,imgArray.length-1);
+    sndRandom=random(0,imgArray.length-1);
     
     console.log(firstRandom);
     console.log(sndRandom);
-    leftImg.src='./loss-wieght-media/' + show.all[firstRandom].imgSrc;
-    rightImg.src='./loss-wieght-media/' + show.all[sndRandom].imgSrc;
+
+    leftSide.src='loss2.jpg';
+    
+    rightImg.src='../loss-wieght-media/' + show.all[sndRandom].image;
 
 show.all[firstRandom].shown++;
 show.all[sndRandom].shown++;
@@ -47,11 +50,16 @@ console.log(show.all)
 }
 render();
 
-
 clickHere.addEventListener('click',handlerFunction);
 function handlerFunction(event){
-    if((event.target.class)=== 'leftImg' || event.target.class === "rightImg")
+    if(((event.target.class)=== 'leftImg' || event.target.class === "rightImg")
+    && counter < numberOfRound)
     {
         render();
+        counter++;
     }
+}
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
