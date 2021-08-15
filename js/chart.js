9e00476baddd287eb3f9c597aea18c6f968c2199
raneemabujamous <raneemabujamous1998@gmail.com>
2021-08-15 10:09:56 +0300

    added

---
+++ js/chart.js
@@ -0,0 +1,45 @@
+function chart() {
+    am4core.useTheme(am4themes_dataviz);
+am4core.useTheme(am4themes_animated);
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['  protein', 'carb', 'vegetables'],
+            datasets: [{
+                label: '# of Votes',
+                data: [25, 25, 50],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    
+    
+    
+}
+
+chart()
