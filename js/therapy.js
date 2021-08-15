1ad1f6868aaacea9a973124c4d9eca3b2ced192a
Eslam Akram <eslam_akram91@yahoo.com>
2021-08-16 01:18:13 +0300

    imagesection created

--- js/therapy.js
+++ js/therapy.js
@@ -1,21 +1,66 @@
 'use strict';
 
 window.addEventListener('load', () => {
-const preload = document.querySelector('.preload');
-preload.classList.add('preload-Finish');
+  const preload = document.querySelector('.preload');
+  preload.classList.add('preload-Finish');
 });
 
 
-let imgArray = [
-    'pablo-merchan-montes-MXovqM130UI-unsplash.jpg',
-    'anna-pelzer-IGfIGP5ONV0-unsplash.jpg',
-    'anna-tukhfatullina-food-photographer-stylist-Mzy-OjtCI70-unsplash.jpg',
-    'brooke-lark-HlNcigvUi4Q-unsplash.jpg',
-    'joseph-gonzalez-zcUgjyqEwe8-unsplash.jpg',
-    'shayna-douglas-GvoVScughvI-unsplash.jpg',
-    'brooke-lark-qdyBKWSzpSI-unsplash.jpg'
-  ];
+let imgBeforeArray = [
+  'story1-before.jpg',
+  'story2-before.jpg',
+  'story3-before.jpg',
+  'story4-before.jpg',
+  'story5-before.png'
 
-  const imageSection = document.getElementById( 'before-after-Section' );
-let leftImage = document.getElementById( 'leftImage' );
-let rightImage = document.getElementById( 'rightImage' );
+];
+
+let imgAfterArray = [
+  'story1-after.png',
+  'story2-after.png',
+  'story3-after.png',
+  'story4-after.png',
+  'story5-after.png'
+
+];
+
+
+const imageSection = document.getElementById('imageSection');
+let beforeImage = document.getElementById('beforeImage');
+let afterImage = document.getElementById('afterImage');
+
+
+function imgStory(story, imageSrc) {
+  this.story = story;
+  this.image = imageSrc;
+  };
+
+imgStory.all = [];
+
+
+console.log(imgStory.all);
+
+
+ function render() {
+
+  for (let i = 0; i < imgBeforeArray.length; i++) {
+
+    beforeImage.src = '../img/imgTherapy/' + this.image;
+
+    afterImage.src = '../img/imgTherapy/' + this.image;
+  }
+
+
+};
+
+this.render();
+
+imageSection.addEventListener('click', clickHandler);
+function clickHandler(e) {
+  if ((e.target.id === 'beforeImage' || e.target.id === 'afterImage')) {
+    this.render();
+
+  }
+
+}
+
