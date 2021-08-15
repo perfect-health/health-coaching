ba705f229de8dc0fb8b2330a06896dfb820d264a
rahafalbakkar <rahaf.a7med1991@gmail.com>
2021-08-15 10:14:01 +0300

    loss-wieght

--- js /loss-weight.js
+++ js /loss-weight.js
@@ -1,65 +1,98 @@
-'use strict';
-// localStorage.clear();
 
-let imgArray =['loss3.jpg',
-'loss2.jpg',' loss4.jpg',
-'loss6.jpg',' loss5.jpg',
-'loss8.jpg',' loss7.jpg',
-'loss9.jpg','index.jpg'];
+ 'use strict';
+ let planArray=[
+     'plan1.jpg',
+     'plan2.jpg',
+     'plan3.jpg',
+     'plan4.jpg',
+     'plan5.jpg'
+ ]
+let all=[];
 
- 
-let all=0;
-let numberOfRound= 8;
-let counter =0;
+// // Create an event listener so that when the delete link is clicked, the removeItemFromCart method is invoked.
+// var table = document.getElementById('cart');
+// table.addEventListener('click', removeItemFromCart);
+// var cart;
 
-let sectionOfImg = document.getElementByID('sectionOfImg');
-let leftSide = document.getElementByID('leftSide');
-// let leftImg = document.getElementByClassName('');
+// function loadCart() {
+//   var cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+//   cart = new Cart(cartItems);
+// }
 
-function show(imgName, imgSrc) {
-    this.imgName = imgName;
-    this.image = imgSrc;
-    this.shown=0;
-        show.all.push(this);
-}
-show.all = [];
+// // Make magic happen --- re-pull the Cart, clear out the screen and re-draw it
+// function renderCart() {
+//   loadCart();
+//   clearCart();
+//   showCart();
+// }
 
-for (let i=0; i<imgArray.length;i++){
-    new show(imgArray[i].split('.')[0],imgArray[i]);   
-}
+// // TODO: Remove all of the rows (tr) in the cart table (tbody)
+// function clearCart() {
+//   document.getElementsByTagName('tr').innerHTML = '';
 
-console.log(show.all);
+  
 
-let firstRandom;
-let sndRandom;
+//   // var table = document.getElementById('cart');
 
-function render(){
-    firstRandom=random(0,imgArray.length-1);
-    sndRandom=random(0,imgArray.length-1);
-    
-    console.log(firstRandom);
-    console.log(sndRandom);
+//   // try somethings new with childNodes
+//   credit : https://www.w3schools.com/jsref/prop_node_childnodes.asp
 
-    leftSide.src='loss2.jpg';
-    
-    rightImg.src='../loss-wieght-media/' + show.all[sndRandom].image;
+//   // var tbody = table.childNodes;
+//   // console.log(tbody[0].childNodes);
+  
+//   credit: https://www.w3schools.com/jsref/prop_node_firstchild.asp
+//   // var nodes = tbody.firstChild;
 
-show.all[firstRandom].shown++;
-show.all[sndRandom].shown++;
-console.log(show.all)
-}
-render();
 
-clickHere.addEventListener('click',handlerFunction);
-function handlerFunction(event){
-    if(((event.target.class)=== 'leftImg' || event.target.class === "rightImg")
-    && counter < numberOfRound)
-    {
-        render();
-        counter++;
-    }
-}
+//   credit: https://www.w3schools.com/jsref/met_node_removechild.asp
+//   // for (var i = 0; i < cart.items.length; i++) {
+//   //   //tbody.removeChild(tbody.firstChild);
+//   // }
+// }
 
-function random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
+// // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
+// function showCart() {
+
+//   // TODO: Find the table body
+//   var tbody = document.getElementsByTagName('tbody');
+
+//   // TODO: Add the TR to the TBODY and each of the TD's to the TR
+//   // TODO: Iterate over the items in the cart
+//   for (var i = 0; i < cart.items.length; i++) {
+//     // TODO: Create a TR
+//     var tr = document.createElement('tr');
+//     tr.setAttribute('id', i);
+//     tbody.appendChild(tr);
+  
+//     // TODO: Create a TD for the delete link, quantity,  and the item
+//     var killTd = document.createElement('td');
+//     killTd.textContent = 'X';
+//     tr.appendChild(killTd);
+
+//     var quantityTd = document.createElement('td');
+//     quantityTd.textContent = cart.items[i].quantity;
+//     tr.appendChild(quantityTd);
+
+//     var itemTd = document.createElement('td');
+//     itemTd.textContent = cart.items[i].product;
+//     tr.appendChild(itemTd);
+//   }
+// }
+
+
+// function removeItemFromCart(event) {
+
+//   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
+//   if (event.target.textContent === 'X') {
+//     cart.removeItem(event.target.id); 
+//     // I think somethings needs to be added between target.''. id
+//   }
+//   // TODO: Save the cart back to local storage
+//   localStorage.setItem('cart', JSON.stringify(cart.items));
+//   // TODO: Re-draw the cart table
+//   renderCart();
+
+// }
+
+// // This will initialize the page and draw the cart on screen
+// renderCart();
